4013d95789d698c980d4d972c5fa7e002da6c13f
Marvin <marvinwanzuita@gmail.com>
2022-08-23 16:57:05 -0300

    Project created

---
+++ backend/src/test/java/com/mwzt/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwzt.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
